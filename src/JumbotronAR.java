5ffcdffe846a028326ef2b944e21fbdb076c7ac7
Dakota S <dakota.soares@execulink.com>
2021-12-29 21:26:15 -0500

    refixed swing jpanel

--- src/JumbotronAR.java
+++ src/JumbotronAR.java
@@ -565,7 +565,7 @@ public class JumbotronAR
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		GridBagLayout gridBagLayout = new GridBagLayout();
-		gridBagLayout.columnWidths = new int[]{0, 178, 0, 552, 0, 178, 0, 0};
+		gridBagLayout.columnWidths = new int[]{5, 178, 0, 552, 0, 178, 0, 0};
 		gridBagLayout.rowHeights = new int[]{125, 0, 97, 0, 0};
 		gridBagLayout.columnWeights = new double[]{0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
 		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 1.0, 0.0, Double.MIN_VALUE};
